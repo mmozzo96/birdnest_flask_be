c488a84d9da79cae66a68ea8db953d366011507d
mmozzo96 <micky.mozzo@gmial.com>
2023-01-05 12:15:16 +0100

    Timestamp isoformat fix

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from database import database
 from flask_cors import cross_origin
+import requests
+import xmltodict
 
 database_connection = database()
 
@@ -21,9 +23,10 @@ def drones_and_pilots():
 @app.route('/drones')
 @cross_origin()
 def drones():
-  drones_and_pilots = database_connection.getDrones()
+  response = requests.get("https://assignments.reaktor.com/birdnest/drones")
+  report = xmltodict.parse(response.content)['report']
 
-  return drones_and_pilots
+  return report['capture']['drone']
 
 
 if __name__=="__main__":
