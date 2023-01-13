677fdabe23e8971ce4eabe64e8a86e4579adb185
mmozzo96 <micky.mozzo@gmial.com>
2023-01-13 10:40:37 +0200

    Stopping job execution after 1h
    To stay into the usage of Heroku free tier dyno the job pauses after 1hr since the last time APIs have been called. Upon call of API the job restart, reseting the timer

--- app.py
+++ app.py
@@ -7,8 +7,22 @@ import xmltodict
 
 database_connection = database()
 
+count = 0
+jobIsPaused = False
+
+def job_function():
+  global count, job, jobIsPaused
+  
+  database_connection.update_database()
+
+  count = count + 1
+  if count == 1800:
+    job.pause()
+    jobIsPaused = True
+
+
 sched = BackgroundScheduler(daemon=True)
-sched.add_job(database_connection.update_database,'interval',seconds=2)
+job = sched.add_job(job_function,'interval', seconds=2, id="AdfaF345we")
 sched.start()
 
 app = Flask(__name__)
@@ -16,15 +30,25 @@ app = Flask(__name__)
 @app.route('/dronesandpilots')
 @cross_origin()
 def drones_and_pilots():
+  global count, job
+
   drones_and_pilots = database_connection.getDrones()
+
+  count = 0
+  job.resume()
 
   return drones_and_pilots
 
 @app.route('/drones')
 @cross_origin()
 def drones():
+  global count, job
+
   response = requests.get("https://assignments.reaktor.com/birdnest/drones")
   report = xmltodict.parse(response.content)['report']
+
+  count = 0
+  job.resume()
 
   return report['capture']['drone']
 
